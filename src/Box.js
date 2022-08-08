fdd39eeb124ff5b893f89504ce19e4a417baad6a
ryan <ryanjgecelter@gmail.com>
2022-08-08 13:36:25 +0300

    add Box component rendered as <ul> by BoxList parent component

---
+++ src/Box.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Box = ({backgroundColor, width, height}) => {
+    return (
+    <div style={{backgroundColor, width, height}}>
+    </div>
+    )
+}
+
+export default Box;
--- src/BoxList.js
+++ src/BoxList.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import NewBoxForm from './NewBoxForm'
+import Box from './Box'
 
 const BoxList = () => {
+    // const [boxes, setBoxes] = useState([<div style={{backgroundColor: 'pink', width:'50px', height: '50px'}}/>, <div style={{backgroundColor: 'pink', width:'50px', height: '50px'}}/>, <div style={{backgroundColor: 'pink', width:'50px', height: '50px'}}/>])
+    const [boxes, setBoxes] = useState([{backgroundColor: 'pink', width:'50px', height: '50px'}, {backgroundColor: 'green', width:'50px', height: '50px'}, {backgroundColor: 'blue', width:'50px', height: '50px'}])
     return (
         <div>
             <h1>Box List!</h1>
+            <ul>
+            {boxes.map(({backgroundColor, width, height}) => <li><Box backgroundColor={backgroundColor} width={width} height={height}/></li>)}
+            </ul>
         </div>
     )
 }
